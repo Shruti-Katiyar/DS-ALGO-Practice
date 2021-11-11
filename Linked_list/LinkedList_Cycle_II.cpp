bdb81fe9d8850bcf4bb208ec11cde917ca620eed
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-11-11 11:31:03 +0530

    cycle

---
+++ Linked_list/LinkedList_Cycle_II.cpp
@@ -0,0 +1,24 @@
+// Linked List Cycle II
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        ListNode*fast=head;
+        ListNode*slow=head;
+        while(fast and fast->next){
+            fast=fast->next->next;
+            slow=slow->next;
+            if(fast==slow){ //if cycle is presentthen slow again head say start                               hogha
+                slow=head;
+                while(slow!=fast){
+                    slow=slow->next;
+                    fast=fast->next;
+                }//where fast and slow meet return that idx
+                return slow;
+                
+            }
+        }
+        //agar cycle nh h return null
+        return NULL;
+        
+    }
+};
