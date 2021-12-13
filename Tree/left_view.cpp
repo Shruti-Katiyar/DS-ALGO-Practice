34195ea1738f41bcf90d87f2ad19c8cf38846f02
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-12-13 19:28:43 +0530

    left

---
+++ Tree/left_view.cpp
@@ -0,0 +1,18 @@
+//Left View of Binary Tree 
+void recursion(Node*root,int level,vector<int>&ans){
+        if(root==NULL)return;
+        if(ans.size()==level)
+            ans.push_back(root->data);
+        recursion(root->left,level+1, ans);
+        recursion(root->right,level+1,ans);
+    }
+    
+vector<int> leftView(Node *root)
+{
+   // Your code here
+    vector<int> ans;
+        recursion(root,0,ans);
+        return ans;
+        
+}
+
