1fe1f81468ae70261e46ad9108a750850c5b5e21
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2021-12-18 16:04:51 +0530

    duplicate

---
+++ Tree/Duplicate_subtree_BinaryTree.cpp
@@ -0,0 +1,31 @@
+//Duplicate subtree in Binary Tree 
+
+class Solution {
+  public:
+    /*This function returns true if the tree contains 
+    a duplicate subtree of size 2 or more else returns false*/
+    unordered_map<string,int>m;
+    string solve(Node *root){
+        if(!root)return "#";
+        string s="";
+        if(!root->right and !root->left){
+            s=to_string(root->data);
+            return s;
+            
+        }
+        s=s + to_string(root->data);
+        s=s + solve(root->left);
+        s=s + solve(root->right);
+        m[s]++;
+        return s;
+    }
+    
+    int dupSub(Node *root) {
+         // code here
+         solve(root);
+         for(auto x:m){
+             if(x.second >=2)return true;
+         }
+         return false;
+    }
+};
