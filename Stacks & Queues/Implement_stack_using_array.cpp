4622a43dd24e028070c7fcfa4b15b90ec795210e
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-01-01 20:32:26 +0530

    arr

---
+++ Stacks & Queues/Implement_stack_using_array.cpp
@@ -0,0 +1,21 @@
+//Implement stack using array (gfg)
+
+//Function to push an integer into the stack.
+void MyStack :: push(int x)
+{
+    // Your Code
+    top=top+1;
+    arr[top]=x;
+}
+
+//Function to remove an item from top of the stack.
+int MyStack :: pop()
+{
+    // Your Code  
+    if(top==-1){
+        return -1;
+    }
+    return arr[top--];
+    
+}
+
