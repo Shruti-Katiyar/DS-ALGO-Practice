3cbc517788f0541b13d871e9b95a29a848c6c239
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-02-04 17:42:33 +0530

    arr

---
+++ Recursion/Number_of_Squareful_Arrays.cpp
@@ -0,0 +1,40 @@
+//Number of Squareful Arrays
+https://leetcode.com/problems/number-of-squareful-arrays/submissions/
+
+class Solution {
+public:
+    int ans=0;
+    void solve(vector<int>&nums,int start){
+        //base
+        if(start>1){
+            int sum=nums[start-1]+nums[start-2];
+            int y=sqrt(sum);
+            if(y*y-sum!=0)return;
+        }
+        if(start==nums.size()){
+            ans++;
+            return;
+        }
+        
+        //rec
+        unordered_map<int,bool>m;
+        for(int end=start;end<nums.size();end++){
+            if(end==start || nums[start]!=nums[end]){
+                if(m.count(nums[end])==0){
+                    swap(nums[start],nums[end]);
+                    solve(nums,start+1);
+                    swap(nums[start],nums[end]);
+                }
+                m[nums[end]]=1;
+            }
+            
+        }     
+                  
+    }
+    int numSquarefulPerms(vector<int>& nums) {
+        sort(nums.begin(),nums.end());
+        solve(nums,0);
+        return ans;
+        
+    }
+};
