a48d4b12be11f5dd7070ad477857ee4da98a7631
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-02-20 22:43:50 +0530

    sumroot to leaf

---
+++ Tree/Sum_Root_To_Leaf_BinaryNumbers.cpp
@@ -0,0 +1,16 @@
+//Sum of Root To Leaf Binary Numbers
+class Solution {
+public:
+      int sol=0;
+    int sumRootToLeaf(TreeNode* root,int num=0) {
+        if(!root)return 0;
+        num *= 2;
+        num+=root->val;
+        if(!root->left&&!root->right)sol+=num;
+        sumRootToLeaf(root->left,num);
+        sumRootToLeaf(root->right,num);
+        return sol;
+    }
+        
+    
+};
