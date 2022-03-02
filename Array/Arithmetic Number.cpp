55c2b0850e211ec2f3230a319f178ac2dc00fe6d
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-02 21:52:18 +0530

    no

---
+++ Array/Arithmetic Number.cpp
@@ -0,0 +1,24 @@
+//Arithmetic Number
+
+https://practice.geeksforgeeks.org/problems/arithmetic-number2815/1#
+
+class Solution{
+public:
+    int inSequence(int a, int b, int c){
+        // code here
+        if(c==0){
+            if(a==b)
+               return 1;
+            else
+               return 0;
+            
+        }
+        else{
+            if((b-a)/c < 0)return 0;
+            else
+            if((b-a)%c==0)return 1;
+            else 
+              return 0;
+        }
+    }
+};
