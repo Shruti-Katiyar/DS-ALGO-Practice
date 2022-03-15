42086d4fccdb23801af8ab462d5049298d9929a7
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-15 22:37:29 +0530

    sub

---
+++ Strings/Is Subsequence.cpp
@@ -0,0 +1,24 @@
+//Is Subsequence
+https://leetcode.com/problems/is-subsequence/
+
+class Solution {
+public:
+    bool isSubsequence(string s, string t) {
+        int i=t.length()-1;
+        int j=s.length()-1;
+        while(i>=0 && j>=0){
+            if(s[j]==t[i]){
+                i--;
+                j--;
+            }
+            else{
+                i--;
+            }
+        }
+        if(j==-1){
+            return true;
+        }
+        return false;
+        
+    }
+};
