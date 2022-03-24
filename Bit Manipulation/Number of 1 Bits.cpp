013717bc71ad74fa634c1689e18b28c27dbd2033
Shruti-Katiyar <shrutikatiyar459@gmail.com>
2022-03-24 13:40:05 +0530

    bit

---
+++ Bit Manipulation/Number of 1 Bits.cpp
@@ -0,0 +1,15 @@
+//Number of 1 Bits
+https://leetcode.com/problems/number-of-1-bits/submissions/
+
+class Solution {
+public:
+    int hammingWeight(uint32_t n) {
+        int cnt=0;  // count of set bit
+        while(n>0){  // iterate until all bits are traversed
+            if((n&1)>0) // check the parity of first bit from right
+                ++cnt;
+            n=n>>1; //n=n/2, shift one bit to right
+        }
+        return cnt;
+    }
+};
